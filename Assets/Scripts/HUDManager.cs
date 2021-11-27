7a627e7a4e8d113073168c49020818529ef0afb1
Ansel69 <ansel003455@gmail.com>
2021-11-27 21:51:59 +0700

    Pertemuan 7
    Save Game, Load game, Movement fix, Obstacle, Camera Shaking

---
+++ Assets/Scripts/HUDManager.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDManager : MonoBehaviour
+{
+    public Image currentEnergy;
+    private GameObject player;
+    private float energy = 200;
+    private float maxEnergy = 200;
+    private float kecepatan;
+    private float kecepatanLari;
+    private float input_x;
+    private float input_z;
+    public Text time;
+    [SerializeField] GameObject pauseMenu;
+    public static bool GameIsPaused = false;
+    public Player playerInstance;
+    private float darah;
+    private float maxDarah = 100f;
+    public Image currentDarah;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        kecepatanLari = player.GetComponent<playermovement>().speed_lari;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        kecepatan = player.GetComponent<playermovement>().kecepatan;
+
+        input_x = player.GetComponent<playermovement>().x;
+
+        input_z = player.GetComponent<playermovement>().z;
+
+        darah = player.GetComponent<sistem_darah>().darah_player;
+
+        EnergyDrain();
+        UpdateEnergy();
+        UpdateTime();
+        ShowPauseMenu();
+        UpdateDarah();
+    }
+
+    private void EnergyDrain()
+    {
+        if(kecepatan == kecepatanLari)
+        {
+            if(input_x > 0 || input_z > 0)
+            {
+                if(energy > 0)
+                {
+                    energy -= 10 * Time.deltaTime;
+                }                
+            }
+            else
+            {
+                if(energy < maxEnergy)
+                {
+                    energy += 15 * Time.deltaTime;
+                }                
+            }
+        }
+
+    }
+
+    private void UpdateEnergy()
+    {
+        float ratio = energy / maxEnergy;
+        currentEnergy.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+
+    private void UpdateTime()
+    {
+        int hours = EnviroSky.instance.GameTime.Hours;
+
+        int minutes = EnviroSky.instance.GameTime.Minutes;
+
+        string gameHours;
+        string gameMinutes;
+
+        if(hours >= 0 && hours <10)
+        {
+            gameHours = "0" + hours.ToString();
+        }
+        else
+        {
+            gameHours = hours.ToString();            
+        }
+
+        if(minutes >= 0 && minutes < 10)
+        {
+            gameMinutes = "0" + minutes.ToString();            
+        }
+        else
+        {
+            gameMinutes = minutes.ToString();
+        }
+
+        time.text = gameHours + " : " + gameMinutes;
+    }
+
+    private void ShowPauseMenu()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        GameIsPaused = false;
+
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause()
+    {
+        pauseMenu.SetActive(true);
+        GameIsPaused = true;
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void SaveGame()
+    {
+        SaveSystem.SavePlayer(playerInstance);
+    }
+
+    private void UpdateDarah()
+    {
+        float ratio = darah / maxDarah;
+        currentDarah.rectTransform.localScale = new Vector3(ratio, 1, 1);
+    }
+}
---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    // Variable
+    [SerializeField] private float kecepatanRotasi = 100f;
+    [SerializeField] private float mouseX, mouseY;
+
+    // References
+    public Transform player, target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX += Input.GetAxis("Mouse X")* kecepatanRotasi;
+        mouseY -= Input.GetAxis("Mouse Y")* kecepatanRotasi;
+
+        mouseY = Mathf.Clamp(mouseY, -35, 60);
+        transform.LookAt(target);
+
+        target.rotation = Quaternion.Euler(mouseY, mouseX, 0);
+        player.rotation = Quaternion.Euler(0, mouseX, 0);    
+    }
+}
